3cc7c232ea475185c5f3c2a03102cf76c45d1ea7
Timo Viisteensaari <timo.viisteensaari@solita.fi>
2013-11-03 16:40:36 +0200

    Refactored controllers

--- project.clj
+++ project.clj
@@ -8,6 +8,7 @@
                  [honeysql "0.4.2"]
                  [postgresql "9.1-901.jdbc4"]
                  [ring/ring-jetty-adapter "1.1.6"]
+                 [ring/ring-json "0.2.0"]
                  [compojure "1.1.3"]
                  [org.clojure/data.json "0.2.3"]
                  [clj-time "0.6.0"]]
