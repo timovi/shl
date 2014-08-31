e26adf90e2039e60e295bd8fc2be0d46aa1e9957
Timo Viisteensaari <timo.viisteensaari@solita.fi>
2014-08-31 13:38:31 +0300

    Controller for standings

---
+++ src/shl/controllers/standings.clj
@@ -0,0 +1,24 @@
+(ns shl.controllers.standings
+  (:use [compojure.core])
+  (:use [ring.util.response])
+  (:require [clojure.string :as str]
+            [ring.util.response :as ring]
+            [clojure.data.json :as json]
+            [clj-time.format :as time]
+            [shl.utils.time :as time-utils]
+            [shl.service.statistics :as service]))
+
+(defn get-tournament-standings [tournamentid]
+  (response (service/get-tournament-standings (Integer/parseInt tournamentid))))
+
+(defn get-conference-standings [conferenceid]
+  (response (service/get-conference-standings (Integer/parseInt conferenceid))))
+
+(defn get-player-standings [playerid]
+  (response (service/get-player-standings (Integer/parseInt playerid))))
+
+(defroutes app-routes
+  (context "/standings" [] (defroutes standings-routes
+    (GET ["/tournament/:id/" :id #"[0-9]+"] [id] (get-tournament-standings id))
+    (GET ["/conference/:id/" :id #"[0-9]+"] [id] (get-conference-standings id))
+    (GET ["/player/:id/" :id #"[0-9]+"] [id] (get-player-standings id)))))
